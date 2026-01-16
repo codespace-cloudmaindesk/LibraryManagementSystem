2b95b0c851d4ceb05fb68c097f380e6f188b83e9
tudinkojhb024 <tudinkojhb024@student.wethinkcode.co.za>
2026-01-17 00:59:39 +0200

    initial commit

---
+++ aspnet-core/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using aspnet_core.Models;
+
+namespace aspnet_core.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<aspnet_core.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ aspnet-core/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace aspnet_core.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool isAvailable { get; set; }
+        public Book()
+        {
+                
+        }
+    }
+}
